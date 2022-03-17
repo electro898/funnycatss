0d86c624642a6ffaa17cf52495048010c2728b76
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-17 22:24:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
         //enter the domain or subdomain where you have WordPress installed 
-      domains: ['https://positivityminds.com/wp-json/wp/v2/posts'],
+      domains: ['positivityminds.com'],
     },
 }
